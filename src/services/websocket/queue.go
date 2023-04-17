83185b06eb56a4b1ac13476a0422556fcee01719
Daniel Rosenfeld <daniel.chid@gmail.com>
2023-04-16 22:54:15 -0400

    changes self member to pointer in Queue

--- src/services/websocket/queue.go
+++ src/services/websocket/queue.go
@@ -9,27 +9,27 @@ type Result struct {
 	Err   error
 }
 
-func (q Queue) enqueue(channel chan Result) {
+func (q *Queue) enqueue(channel chan Result) {
 	println(q.list)
 	q.list = append(q.list, channel)
 	println(q.list)
 	println(len(q.list))
 }
 
-func (q Queue) dequeue() chan Result {
+func (q *Queue) dequeue() chan Result {
 	channel := q.list[0]
 	q.list = q.list[1:]
 	return channel
 }
 
-func (q Queue) isEmpty() bool {
+func (q *Queue) isEmpty() bool {
 	return len(q.list) == 0
 }
 
-func (q Queue) isNotEmpty() bool {
+func (q *Queue) isNotEmpty() bool {
 	return !q.isEmpty()
 }
 
-func (q Queue) size() int {
+func (q *Queue) size() int {
 	return len(q.list)
 }
